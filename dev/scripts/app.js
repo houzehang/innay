8b3d4cfed3165c994a3f90e01fa6d1401fbcc167
Owen <dreamflywang@foxmail.com>
2018-04-29 14:45:41 +0800

    bugfix

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,9 +1,76 @@
-let tmpl    = require("./jquery.tmpl.js");
-let Q 		= require("q");
-let Const 	= require("../const.js");
+let tmpl    	= require("./jquery.tmpl.js");
+let Q 			= require("q");
+let Const 		= require("../const.js");
+let Calendar	= require("./calendar")
 class App {
 	constructor() {
 		console.log("app...")
+		this.data 		= {}
+		this.$calendar 	= new Calendar(this)
+		this.__bind()
+	}
+
+	__bind() {
+		$(".start-btn").on("click",()=>{
+			$(".enter-layer").show()
+			setTimeout(()=>{
+				$(".enter-layer").addClass("show")
+			},100)
+		})
+		$(".mask").on("click",".close-btn,.cancel-btn",(event)=>{
+			let target = $(event.currentTarget).parents(".mask")
+			target.removeClass("show")
+			setTimeout(()=>{
+				target.hide()
+			},300)
+		})
+		$(".enter-btn").on("click",(event)=>{
+			$(".page").addClass("next")
+			$(".mask").removeClass("show")
+			setTimeout(()=>{
+				$(".mask").hide()
+			},300)
+		})
+		$(".switch").on("click",(event)=>{
+			let target = $(event.currentTarget).parents(".buttons")
+			target.toggleClass("open")
+		})
+		$(".back-btn").on("click",(event)=>{
+			$(".back-layer").show()
+			setTimeout(()=>{
+				$(".back-layer").addClass("show")
+			},100)
+		})
+		$(".ok-btn").on("click",()=>{
+			$(".page").removeClass("next")
+		})
+		$(".course-close").on("click",()=>{
+			$(".exit-layer").show()
+			setTimeout(()=>{
+				$(".exit-layer").addClass("show")
+			},100)
+		})
+		$(".exit-btn").on("click",()=>{
+			$(".page").removeClass("next")
+			$(".exit-layer").removeClass("show")
+			setTimeout(()=>{
+				$(".exit-layer").hide()
+			},300)
+		})
+		$(".gift,.handsup").on("click",(event)=>{
+			let target = $(event.currentTarget)
+			target.toggleClass("disabled")
+		})
+	}
+
+	onDateChanged(date) {
+		let choosed = this.data.calendar_data.choosed
+		let today   = this.data.calendar_data.today
+		if (this.$calendar.__is_sameday(choosed, today)) {
+
+		} else {
+
+		}
 	}
 }
 
--- main.js
+++ main.js
@@ -17,48 +17,55 @@ if (setupEvents.handleSquirrelEvent()) {
 // 初始化主框架
 const {session,app,BrowserWindow,ipcMain} = require('electron');
 app.on('ready', ()=>{
-    // 创建主窗口，配置详见Electron官方文档
-    let $main = new BrowserWindow({width: 1440, height: 900, 
-        resizable: true,
-        center: true,
-        frame: true,
-        autoHideMenuBar: true,
-        webPreferences : {
-            webSecurity: false,
-            javascript: true,
-            plugins: true
-            // nodeIntegration: false
+    let mainWindow
+    function createWindow() {
+        // 创建主窗口，配置详见Electron官方文档
+        let $main = new BrowserWindow({width: 1024, height: 768, 
+            resizable: false,
+            center: true,
+            frame: true,
+            autoHideMenuBar: true,
+            webPreferences : {
+                webSecurity: false,
+                javascript: true,
+                plugins: true
+                // nodeIntegration: false
+            }
+        })
+        $main.webContents.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36');
+        // 加载本地入口文件，用于生成主UI界面
+        $main.loadURL(`file://${__dirname}/dist/index.html`)
+        // $main.loadURL('https://wx.qq.com')
+        
+        if (DEBUG) {
+            // 打开调试器
+            $main.webContents.openDevTools();
         }
-    })
-    $main.webContents.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36');
-    // 加载本地入口文件，用于生成主UI界面
-    $main.loadURL(`file://${__dirname}/dist/index.html`)
-    // $main.loadURL('https://wx.qq.com')
-    
-    if (DEBUG) {
-        // 打开调试器
-        $main.webContents.openDevTools();
+        $main.webContents.on('will-navigate', (ev, url) => {
+            ev.preventDefault();
+        });
+        $main.on('closed', function (event) {
+            mainWindow = null
+        })
+        mainWindow = $main
     }
-    $main.webContents.on('will-navigate', (ev, url) => {
-        ev.preventDefault();
-    });
-    $main.on('closed', function () {
-        $main = null
-    })
+    createWindow()
 	// Modify the user agent for all requests to the following urls.
 	const filter = {
 		urls: []
-	}
+    }
+
+    app.on('window-all-closed', function () {
+        if (process.platform !== 'darwin') {
+            app.quit()
+            app.exit(0)
+            mainWindow = null
+        }
+    })
+
+    app.on('activate', function () {
+        if (mainWindow === null) {
+            createWindow()
+        }
+    })
 });
-
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
-
-app.on('activate', function () {
-    if (mainWindow === null) {
-        createWindow()
-    }
-})
