89be13cb15703758a5c79b17bc7f7fbd44a9da66
Owen <dreamflywang@foxmail.com>
2019-03-25 20:00:46 +0800

    bugfix

--- dev/scripts/containers/main.teacher.page.js
+++ dev/scripts/containers/main.teacher.page.js
@@ -106,25 +106,9 @@ class Main extends React.Component {
 	}
 
 	onStartRoom(data) {
-		// net.getRoomInfo(data.channel_id).then((result) => {
-		// 	for (let key in result) {
-		// 		data[key] = result[key]
-		// 	}
-		// 	data.user_id = this.props.account.id
-		// 	data.app_id  = Conf.AGORA_APPID
-		// 	console.log("set up open classroom",data)
-		// 	ipcRenderer.send('open-classroom', data);
-		// }).done()
-		// return
-		// 判断最近1小时内是否下载过课程包，如果下载过则不提示下载
-		// let lastest_download = storage.get(`download_${data.en_name}`)
-		// if (lastest_download) {
-		// 	let delay = new Date().getTime() - lastest_download
-		// 	if (delay <= this.$cache_valid_time) {
-		// 		this.__onStartRoom(data)
-		// 		return
-		// 	}
-		// }
+		if (window.cc == undefined) {
+			$require("./libs/cocos2d-js-v1.1-min.js")
+		}
 		this.onDownload(data, true)
 	}
 
