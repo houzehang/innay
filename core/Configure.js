7a5546ac30d04787f7f3231a93f8a1b0ba029b49
houzehang <houzehang@youshi-inc.com>
2019-05-23 23:56:47 +0800

    通过proxy区分测试与正式环境

--- core/Configure.js
+++ core/Configure.js
@@ -1,13 +1,13 @@
 import { app } from "electron"
 import path from 'path'
-import { DEBUG } from '../env'
+import { DEBUG, TEST } from '../env'
 export const APP_PATH 				= app.getPath("userData")
 export const PACKAGE_PATH 			= path.join(APP_PATH, "packages")
 export const ASSETS_PATH  	 		= path.join(APP_PATH, "appassets")
 export const LOG_PATH				= path.join(APP_PATH, "system.log")
 export const INSTALLED_META_FILE 	= "lastest-installed-meta"
 export const PACKAGE_META_FILE 		= "lastest-downloaded-meta"
-export const PROXY 					= "mingxi"
+export const PROXY 					= (DEBUG || TEST) ? "mingxi-test" : "mingxi"
 export const WINDOW_ADAPTER 		= DEBUG ? {
 	"classroom-ui" : "http://localhost:3030",
 	"liveroom"     : "http://localhost:3000/app?from=turbo"
