6f78f89ace8133c39ee582b11d18f2cb6205ed5f
houzehang <houzehang@youshi-inc.com>
2020-07-05 20:48:28 +0800

    bugfix

--- dev/scripts/components/matter.js
+++ dev/scripts/components/matter.js
@@ -16,14 +16,15 @@ class MatterPage extends React.Component {
             
         }}>
             <div className="matter-content">
+				{/* 千图网 */}
 				<div className="matter-item">
-					<div className="logo"></div>
+					<div className="logo qiantu"></div>
 					<div className="matter-item-title">
 						<span className="icon icon-rocket" onClick={()=>{
 							context.linkTo('https://www.58pic.com/')
 						}}></span>
 					</div>
-					<div>登陆方式:
+					<div>登陆方式：
 						<div className="login-style qq"></div>
 					</div>
 					<div>账号：2415343397
@@ -33,13 +34,69 @@ class MatterPage extends React.Component {
 					</div>
 					<div>密码：{this.state.view_open_qiantu ? 'chengxubao598' : '************'} 
 						<span className="copy view icon icon-eye" onClick={()=>{
-							// context.linkTo('https://www.58pic.com/')
+							let view_open_qiantu = this.state.view_open_qiantu
 							this.setState({
-								view_open_qiantu: true
+								view_open_qiantu: !view_open_qiantu
 							})
 						}}></span>
 						<span className='copy' onClick={()=>{
 							clipboard.writeText('chengxubao598')
+						}}>复制</span>
+					</div>
+				</div>
+				{/* 6m5m */}
+				<div className="matter-item">
+					<div className="logo _6m5m"></div>
+					<div className="matter-item-title">
+						<span className="icon icon-rocket" onClick={()=>{
+							context.linkTo('http://www.6m5m.com/')
+						}}></span>
+					</div>
+					<div>登陆方式：
+						<div className="login-style qq"></div>
+					</div>
+					<div>账号：2415343397
+						<span className='copy' onClick={()=>{
+							clipboard.writeText('2415343397')
+						}}>复制</span>
+					</div>
+					<div>密码：{this.state.view_open_6m5m ? 'chengxubao598' : '************'} 
+						<span className="copy view icon icon-eye" onClick={()=>{
+							let view_open_6m5m = this.state.view_open_6m5m
+							this.setState({
+								view_open_6m5m: !view_open_6m5m
+							})
+						}}></span>
+						<span className='copy' onClick={()=>{
+							clipboard.writeText('chengxubao598')
+						}}>复制</span>
+					</div>
+				</div>
+				{/* 爱给网 */}
+				<div className="matter-item">
+					<div className="logo aigei"></div>
+					<div className="matter-item-title">
+						<span className="icon icon-rocket" onClick={()=>{
+							context.linkTo('http://www.aigei.com/')
+						}}></span>
+					</div>
+					<div>登陆方式：暂无
+						{/* <div className="login-style">-</div> */}
+					</div>
+					<div>账号：暂无
+						<span className='copy' onClick={()=>{
+							clipboard.writeText('2415343397')
+						}}>复制</span>
+					</div>
+					<div>密码：{this.state.view_open_aigei ? '暂无' : '************'} 
+						<span className="copy view icon icon-eye" onClick={()=>{
+							let view_open_aigei = this.state.view_open_aigei
+							this.setState({
+								view_open_aigei: !view_open_aigei
+							})
+						}}></span>
+						<span className='copy' onClick={()=>{
+							clipboard.writeText('暂无')
 						}}>复制</span>
 					</div>
 				</div>
